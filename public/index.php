2016153256bd174fe36e273fdee2e43dbdcaa923
Léo Peltier <contact@leo-peltier.fr>
2015-09-14 16:51:26 +0200

    Read conf from ~/.config/woland.json.

--- public/index.php
+++ public/index.php
@@ -4,7 +4,6 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 error_reporting(-1);
 
-$app = new Woland\Application([
-    'public' => __DIR__,
-]);
+$confPath = posix_getpwuid(posix_getuid())['dir'] . '/.config/woland.json';
+$app = new Woland\Application(json_decode(file_get_contents($confPath), true));
 $app(Zend\Diactoros\ServerRequestFactory::fromGlobals());
